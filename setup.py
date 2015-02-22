9ac95ff6af9c79dd9cd4aa2a2767bc90f3417d6d
Silas Boyd-Wickizer <silasbw@gmail.com>
2015-02-22 15:58:34 -0800

    Version 0.2

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ except ImportError:
   from distutils.core import setup
 
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
   author='Silas Boyd-Wickizer',
   author_email='silas@godaddy.com',
@@ -17,14 +20,15 @@ setup(
     'Programming Language :: Python :: 2.7',
   ),
   description='Transaction barriers for Django and Celery.',
-  download_url='https://github.com/godaddy/django_transaction_barrier/tarball/0.1',
+  download_url='https://github.com/godaddy/django_transaction_barrier/tarball/0.2',
   include_package_data=True,
   install_requires=('Django>=1.4.0,<1.8.0','celery>=3.0.0,<4.0.0'),
   keywords=('django', 'transaction', 'celery', 'atomic'),
   license=open('LICENSE.txt').read(),
+  long_description=long_description,
   name='django-transaction-barrier',
   packages=('django_transaction_barrier', 'django_transaction_barrier.migrations'),
   url='https://github.com/godaddy/django_transaction_barrier',
-  version='0.1',
+  version='0.2',
   zip_safe=True,
 )
